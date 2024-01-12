5439ede08ccc77c451ab195bb5d1c850c53aae97
shemmee <chaimalamirin@gmail.com>
2024-01-12 16:06:17 +0000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import Login from './pages/Login'
+//import Login from './pages/Login'
+//import Register from './pages/Register'
+import Home from './pages/Home'
 import './App.scss'
 
 function App() {
 
   return (
     <>
-      <Login />
+    <Home />
     </>
   )
 }
