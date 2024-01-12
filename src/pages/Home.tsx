7ec02a25649f5fb5b3e5316a12ad41dd0e113e37
shemmee <chaimalamirin@gmail.com>
2024-01-12 22:08:36 +0000

    code: add Chats Component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Sidebar from "../components/Sidebar";
+import Chats from "../components/Chats"
 
 const Home:React.FC = () => {
   return (
     <div className="home">
       <Sidebar />
+      <Chats />
     </div>
   );
 };
