8ebaf9afb9ff15d4c9b94a47b8ca299431c18a7c
shemmee <chaimalamirin@gmail.com>
2024-01-12 23:52:26 +0000

    code: Chat Component + styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Sidebar from "../components/Sidebar";
-import Chats from "../components/Chats"
+import Chats from "../components/Chats";
+import Chat from "../components/Chat"
 
 const Home:React.FC = () => {
   return (
     <div className="home">
       <Sidebar />
       <Chats />
+      <Chat />
     </div>
   );
 };
