80efb086fbdcd6beef97fea873e2262d6fb73493
shemmee <chaimalamirin@gmail.com>
2024-01-12 16:05:14 +0000

    code: Home Page + styling

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Sidebar from "../components/Sidebar";
+
+const Home:React.FC = () => {
+  return (
+    <div className="home">
+      <Sidebar />
+    </div>
+  );
+};
+
+export default Home;
