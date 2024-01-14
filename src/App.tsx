98acbe1aab5398282003f1acb02838184ab83e42
shemmee <chaimalamirin@gmail.com>
2024-01-14 21:51:17 +0000

    feat: set /register as the default route

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-//import Login from './pages/Login'
-//import Register from './pages/Register'
-import Home from './pages/Home'
-import './App.scss'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Home from './pages/Home';
+
+import './App.scss';
 
 function App() {
-
   return (
-    <>
-    <Home />
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
