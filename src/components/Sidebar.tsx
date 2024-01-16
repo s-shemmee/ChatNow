5aa4112a2ba64e354a7ea1688253e6e42a295874
shemmee <chaimalamirin@gmail.com>
2024-01-16 15:35:34 +0000

    Implementing Sign out Authentication Logic

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,25 +1,49 @@
 import React from "react";
 import logo from "../assets/logo.png";
+import { auth } from "../firebase";
+import { signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
 import SmsRoundedIcon from '@mui/icons-material/SmsRounded';
 import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
 import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
+import { IconButton } from "@mui/material";
 
 const Sidebar:React.FC = () => {
+
+  const navigate = useNavigate();
+  function handleSignOut() {
+    signOut(auth).then(() => {
+      localStorage.removeItem('token');
+      localStorage.removeItem('user');
+      navigate("/login");
+    }).catch((error) => {
+      console.error('Error:', error);
+    });
+  }
+
   return (
     <div className="sidebar">
       <div className="logo">
         <img src={logo} alt="" />
       </div>
       <div className="sidebarMenu">
-        <HomeRoundedIcon className="menuIcon"/>
-        <SmsRoundedIcon className="menuIcon"/>
-        <SettingsRoundedIcon className="menuIcon"/>
+        <IconButton>
+          <HomeRoundedIcon className="sidebarIcon"/>
+        </IconButton>
+        <IconButton>
+          <SmsRoundedIcon className="sidebarIcon"/>
+        </IconButton>
+        <IconButton>
+          <SettingsRoundedIcon className="sidebarIcon"/>
+        </IconButton>
       </div>
 
-      <div className="user">
+      <div className="sidebarUser">
         <img src="https://picsum.photos/200" alt="avatar" className="userImg"/>
-        <LogoutRoundedIcon className="userIcon"/>
+        <IconButton onClick={handleSignOut}>
+          <LogoutRoundedIcon className="userSignOut"/>
+        </IconButton>
       </div>
 
     </div>
