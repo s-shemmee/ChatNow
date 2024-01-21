f90bd09f28670e09549db2bfe05cd856200fbaaa
shemmee <chaimalamirin@gmail.com>
2024-01-21 22:02:14 +0000

    add Messages component

---
+++ src/components/Messages.tsx
@@ -0,0 +1,16 @@
+import Message from "./Message"
+
+const Messages = () => {
+  return (
+    <div className="Messages">
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+    </div>
+  )
+}
+
+export default Messages
