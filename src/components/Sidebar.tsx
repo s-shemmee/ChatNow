c14cb9c89af6a985e3eb28924e173742fdaeb607
shemmee <chaimalamirin@gmail.com>
2024-01-23 16:02:03 +0000

    update: Log Out Authentication logic

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -7,20 +7,20 @@ import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
 import SmsRoundedIcon from '@mui/icons-material/SmsRounded';
 import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
 import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
-import { IconButton } from "@mui/material";
+import { IconButton, Tooltip } from "@mui/material";
 
-const Sidebar:React.FC = () => {
-
+const Sidebar: React.FC = () => {
   const navigate = useNavigate();
-  function handleSignOut() {
-    signOut(auth).then(() => {
-      localStorage.removeItem('token');
-      localStorage.removeItem('user');
-      navigate("/login");
-    }).catch((error) => {
-      console.error('Error:', error);
-    });
-  }
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      // Redirect to the login page after successful logout
+      navigate('/login');
+    } catch (error: Error ) {
+      console.error('Logout failed:', error.message || 'An error occurred');
+    }
+  };
 
   return (
     <div className="sidebar">
@@ -28,24 +28,31 @@ const Sidebar:React.FC = () => {
         <img src={logo} alt="" />
       </div>
       <div className="sidebarMenu">
-        <IconButton>
-          <HomeRoundedIcon className="sidebarIcon"/>
-        </IconButton>
-        <IconButton>
-          <SmsRoundedIcon className="sidebarIcon"/>
-        </IconButton>
-        <IconButton>
-          <SettingsRoundedIcon className="sidebarIcon"/>
-        </IconButton>
+        <Tooltip title="Home">
+          <IconButton>
+            <HomeRoundedIcon className="sidebarIcon" />
+          </IconButton>
+        </Tooltip>
+        <Tooltip title="Messages">
+          <IconButton>
+            <SmsRoundedIcon className="sidebarIcon" />
+          </IconButton>
+        </Tooltip>
+        <Tooltip title="Settings">
+          <IconButton>
+            <SettingsRoundedIcon className="sidebarIcon" />
+          </IconButton>
+        </Tooltip>
       </div>
 
       <div className="sidebarUser">
-        <img src="https://picsum.photos/200" alt="avatar" className="userImg"/>
-        <IconButton onClick={handleSignOut}>
-          <LogoutRoundedIcon className="userSignOut"/>
-        </IconButton>
+        <img src="https://picsum.photos/200" alt="avatar" className="userImg" />
+        <Tooltip title="Logout">
+          <IconButton onClick={handleLogout}>
+            <LogoutRoundedIcon className="userSignOut" />
+          </IconButton>
+        </Tooltip>
       </div>
-
     </div>
   );
 };
