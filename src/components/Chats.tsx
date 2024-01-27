d62ff218f4d3cb662c7f490e28ce88879b8d9800
shemmee <chaimalamirin@gmail.com>
2024-01-27 20:27:18 +0000

    feat: search bar integrated with users db

--- src/components/Chats.tsx
+++ src/components/Chats.tsx
@@ -1,19 +1,13 @@
 import React from "react";
-import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
-import { IconButton } from "@mui/material";
+import Searchbar from "../components/Searchbar";
 
 const Chats:React.FC = () => {
   return (
     <div className="chats">
       {/* searchbar*/}
-      <div className="searchbar">
-        <input type="text" name="searchbar" placeholder="Search for users..." />
-        <IconButton>
-          <SearchRoundedIcon className="searchIcon"/>
-        </IconButton>
-      </div>
+      <Searchbar />
 
-      {/* chats*/}
+      {/* chats
       <div className="chatsContainer">
         <div className="chatCard">
           <div className="chatUser">
@@ -27,31 +21,8 @@ const Chats:React.FC = () => {
             1:00pm
           </span>
         </div>
-        <div className="chatCard">
-          <div className="chatUser">
-            <img src="https://picsum.photos/200" alt="" className="chatImg"/>
-            <div className="chatInfo">
-              <h3 className="chatName">Chat 1</h3>
-              <p className="chatMsg">This is the last message</p>
-            </div>
-          </div>
-          <span className="chatTime">
-            1:00pm
-          </span>
-        </div>
-        <div className="chatCard">
-          <div className="chatUser">
-            <img src="https://picsum.photos/200" alt="" className="chatImg"/>
-            <div className="chatInfo">
-              <h3 className="chatName">Chat 1</h3>
-              <p className="chatMsg">This is the last message</p>
-            </div>
-          </div>
-          <span className="chatTime">
-            1:00pm
-          </span>
-        </div>
       </div>
+      */}
     </div>
   );
 };
