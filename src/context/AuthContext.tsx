9b967fac254eba8bdc5577618d6cde24f3e24845
shemmee <chaimalamirin@gmail.com>
2024-01-30 16:01:01 +0000

    ref: update

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,25 +1,27 @@
 import React, { createContext, useEffect, useState } from "react";
 import { auth } from "../firebase";
-import { onAuthStateChanged } from "firebase/auth";
+import { onAuthStateChanged, User } from "firebase/auth";
 
-export const AuthContext = createContext(null);
+export const AuthContext = createContext<User | null>(null);
 
 type AuthProviderProps = {
   children: React.ReactNode;
 };
 
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState({});
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
 
   useEffect(() => {
     const unsub = onAuthStateChanged(auth, (user) => {
       setCurrentUser(user);
-      console.log(user);
+      console.log("User state after reload:", user);
     });
     return () => unsub();
   }, []);
-
-  return <AuthContext.Provider value={currentUser}>{children}</AuthContext.Provider>;
+  // Return the user and a function to logout
+  return (
+    <AuthContext.Provider value={currentUser}>{children}</AuthContext.Provider>
+  );
 };
 
 export default AuthContext;
