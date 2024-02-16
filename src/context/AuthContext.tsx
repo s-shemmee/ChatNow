82ac07df3f09b19e78e4a74f36eb1b46bb1f8dee
shemmee <chaimalamirin@gmail.com>
2024-02-16 23:37:30 +0000

    Update: online status in Firestore when user signs in and out

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,7 +1,8 @@
 import React, { createContext, useEffect, useState } from "react";
 import { auth } from "../firebase";
 import { onAuthStateChanged, User } from "firebase/auth";
-import 'ldrs/dotStream'; 
+import { updateDoc, doc } from "firebase/firestore";
+import { db } from "../firebase";
 
 interface DotStreamProps extends React.HTMLAttributes<HTMLElement> {
   size?: string;
@@ -29,14 +30,39 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const unsub = onAuthStateChanged(auth, (user) => {
+    const unsub = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        // User is signed in, update online status to true in Firestore
+        try {
+          await updateDoc(doc(db, "users", user.uid), {
+            online: true,
+          });
+        } catch (error) {
+          console.error("Error updating online status:", error);
+        }
+      }
+
       setCurrentUser(user);
       setLoading(false); // Set loading to false once the authentication state is determined
       console.log("User state after reload:", user);
     });
 
-    return () => unsub();
-  }, []);
+    // Cleanup function
+    return () => {
+      if (currentUser) {
+        // User is signed out, update online status to false in Firestore
+        try {
+          updateDoc(doc(db, "users", currentUser.uid), {
+            online: false,
+          });
+        } catch (error) {
+          console.error("Error updating online status:", error);
+        }
+      }
+
+      unsub(); // Unsubscribe from onAuthStateChanged
+    };
+  }, [currentUser]);
 
   // Return loading state and user information
   if (loading) {
