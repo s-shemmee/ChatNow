73bd50f6def95767564c3f007565a2b0ff8ea737
shemmee <chaimalamirin@gmail.com>
2024-02-17 23:16:31 +0000

    Ref: Messages component to fetch messages from Firestore

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -1,16 +1,41 @@
-import Message from "./Message"
+import React, { useContext, useEffect, useState } from "react";
+import { doc, onSnapshot } from "firebase/firestore";
+import { db } from "../firebase";
+import Message from "./Message";
+import { ChatContext } from "../context/ChatContext";
 
-const Messages = () => {
-  return (
-    <div className="Messages">
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-    </div>
-  )
+interface MessageData {
+  id: string;
+  senderId: string;
+  senderName: string;
+  senderAvatar: string;
+  text: string;
+  timestamp?: Date;
 }
 
-export default Messages
+const Messages: React.FC = () => {
+  const [messages, setMessages] = useState<MessageData[]>([]);
+  const { state } = useContext(ChatContext);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(doc(db, "chats", state.chatId), (doc) => {
+      doc.exists() && setMessages(doc.data()?.messages || []);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, [state.chatId]);
+
+  console.log(state.chatId, messages);
+
+  return (
+    <div className="Messages">
+      {messages.map((message) => (
+        <Message key={message.id} message={message} />
+      ))}
+    </div>
+  );
+};
+
+export default Messages;
