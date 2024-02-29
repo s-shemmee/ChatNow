f4e8319399f180e9b09d5e29e1545a115f912c3b
shemmee <chaimalamirin@gmail.com>
2024-02-29 16:59:22 +0000

    Remove loading state from Sidebar component

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import logo from "../assets/logo.png";
 import { auth, db } from "../firebase";
 import { doc, updateDoc } from "firebase/firestore";
@@ -10,7 +10,6 @@ import ArchiveRoundedIcon from "@mui/icons-material/ArchiveRounded";
 import SettingsRoundedIcon from "@mui/icons-material/SettingsRounded";
 import LogoutRoundedIcon from "@mui/icons-material/LogoutRounded";
 import { IconButton, Tooltip } from "@mui/material";
-import LoadingScreen from "./LoadingScreen";
 
 interface SidebarProps {
   onHomeClick: () => void;
@@ -19,14 +18,9 @@ interface SidebarProps {
 const Sidebar: React.FC<SidebarProps> = ({ onHomeClick }) => {
   const currentUser = useContext(AuthContext);
   const navigate = useNavigate();
-  const [loading, setLoading] = useState(false);
 
   const handleLogout = async () => {
     try {
-      // Set loading to true before the asynchronous operations
-      // This will trigger the loading screen
-      setLoading(true);
-
       // Set the online status to false in Firestore
       const usersCollectionRef = doc(db, "users", currentUser?.uid || "");
       await updateDoc(usersCollectionRef, {
@@ -40,21 +34,12 @@ const Sidebar: React.FC<SidebarProps> = ({ onHomeClick }) => {
       navigate("/login");
     } catch (error) {
       console.error("Logout failed:", (error as Error).message || "An error occurred");
-    } finally {
-      // Set loading back to false after the asynchronous operations
-      // This will hide the loading screen
-      setLoading(false);
     }
   };
 
   const handleClickHome = () => {
     onHomeClick(); // Call the onHomeClick callback to clear the selected chat
   };
-
-  if (loading) {
-    // Render the LoadingScreen component if loading is true
-    return <LoadingScreen />;
-  }
 
   return (
     <div className="sidebar">
